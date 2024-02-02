0c145c21466103f0f2cd0a204b2797ca623c1e53
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2024-02-02 11:55:49 -0300

    Adding sliders to GUI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,17 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+        juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +39,12 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    CustomRotarySlider peakFreqSlider, peakGainSlider, peakQSlider, lowCutFreqSlider, highCutFreqSlider, 
+                       lowCutShapeSlider, highCutShapeSlider;
+
+    std::vector<juce::Component*> getComps();
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
